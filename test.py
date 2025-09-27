0f4dca26d8312b3956ad91fbc893e5cb16621975
Aayush <aayushbokde31@gmail.com>
2025-09-27 19:16:24 +0530

    test

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-import langchain
-print(langchain.__version__)
+from langchain_openrouter import OpenRouterEmbeddings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+embedding = OpenRouterEmbeddings(
+    model="text-embedding-3-small",
+    openrouter_api_key=os.getenv("OPENROUTER_API_KEY")
+)
+
+result = embedding.embed_query("what is the capital of India?")
+print(result)
